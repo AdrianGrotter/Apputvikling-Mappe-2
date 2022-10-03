61ee5d8bb772619a75cff5d2fbb2471b4f0f5687
JuanMephisto <adrian@grotter.net>
2022-10-03 12:44:00 +0200

    Expanded Friend.java class

--- app/src/main/java/com/example/s354378_mappe2/Friend.java
+++ app/src/main/java/com/example/s354378_mappe2/Friend.java
@@ -1,6 +1,40 @@
 package com.example.s354378_mappe2;
 
 public class Friend {
+    long ID;
     String first;
     String last;
+
+    public Friend() {
+    }
+
+    public Friend(long ID, String first, String last) {
+        this.ID = ID;
+        this.first = first;
+        this.last = last;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
 }
