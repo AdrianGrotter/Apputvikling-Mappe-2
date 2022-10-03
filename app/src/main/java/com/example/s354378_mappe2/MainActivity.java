31da3143efbda2d2150c00433dc18d1997f57f98
JuanMephisto <adrian@grotter.net>
2022-10-03 12:36:27 +0200

    Implemented btnSubmit method

--- app/src/main/java/com/example/s354378_mappe2/MainActivity.java
+++ app/src/main/java/com/example/s354378_mappe2/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.s354378_mappe2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
+    EditText firstName;
+    EditText lastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstName = (EditText) findViewById(R.id.inputFirstName);
+        lastName = (EditText) findViewById(R.id.inputLastName);
+
+        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Friend newFriend = new Friend();
+                newFriend.first = firstName.getText().toString();
+                newFriend.last = lastName.getText().toString();
+                System.out.println(newFriend.first + " " + newFriend.last);
+            }
+        });
     }
 }
