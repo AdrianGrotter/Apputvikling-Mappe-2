ebe4df6936e9ff8cca791b9acd1bb5189cfb2dac
JuanMephisto <adrian@grotter.net>
2022-10-06 20:56:32 +0200

    Added new activity and moved functionality from main activity. Added new buttons on main activity to enable navigating to other activities

--- app/src/main/java/com/example/s354378_mappe2/MainActivity.java
+++ app/src/main/java/com/example/s354378_mappe2/MainActivity.java
@@ -2,24 +2,13 @@ package com.example.s354378_mappe2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    EditText firstName;
-    EditText lastName;
-    EditText phone;
-    TextView contactOutput;
 
-    DBHandler dbHelper;
-    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,56 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstName = (EditText) findViewById(R.id.inputFirstName);
-        lastName = (EditText) findViewById(R.id.inputLastName);
-        phone = (EditText) findViewById(R.id.inputPhone);
+        Button btnRegistrer = (Button) findViewById(R.id.btnCreateContact);
 
-        contactOutput = (TextView) findViewById(R.id.contactOutput);
-
-
-        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
-        Button btnShowContacts = (Button) findViewById(R.id.btnShowContacts);
-
-
-        dbHelper = new DBHandler(this);
-        db = dbHelper.getWritableDatabase();
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        btnRegistrer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Contact newContact = new Contact();
-                newContact.first = firstName.getText().toString();
-                newContact.last = lastName.getText().toString();
-                newContact.phone = phone.getText().toString();
-
-                System.out.println(newContact.first + " " + newContact.last + " " + newContact+phone);
-
-                dbHelper.addContact(db, newContact);
-            }
-        });
-
-        btnShowContacts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String output = "";
-                List<Contact> contactList= dbHelper.retrieveAllContacts(db);
-                for (Contact myContact : contactList){
-                    /*
-                    output += "Id: "+ myContact.get_ID() +
-                            ", Fornavn: "+myContact.getFirst() +
-                            ", Etternavn: "+ myContact.getLast() +
-                            ", Telefonnummer: "+ myContact.getPhone() + "\n";
-                    */
-                    output += myContact.get_ID() + ", "+ myContact.getFirst() + " " + myContact.getLast() + ", " + myContact.getPhone() + "\n";
-                }
-                contactOutput.setText(output);
+                activityAddContacts();
             }
         });
     }
 
-    @Override
-    protected void onDestroy(){
-        dbHelper.close();
-        super.onDestroy();
+    private void activityAddContacts() {
+        Intent myIntent = new Intent(this, createContactActivity.class);
+        startActivity(myIntent);
     }
 }
---
+++ app/src/main/java/com/example/s354378_mappe2/createContactActivity.java
@@ -0,0 +1,97 @@
+package com.example.s354378_mappe2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class createContactActivity extends AppCompatActivity {
+    EditText firstName;
+    EditText lastName;
+    EditText phone;
+    TextView contactOutput;
+
+    DBHandler dbHelper;
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_contact);
+
+        Button btnMain = (Button) findViewById(R.id.btnMain);
+        firstName = (EditText) findViewById(R.id.inputFirstName);
+        lastName = (EditText) findViewById(R.id.inputLastName);
+        phone = (EditText) findViewById(R.id.inputPhone);
+
+        contactOutput = (TextView) findViewById(R.id.contactOutput);
+
+
+        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
+        Button btnShowContacts = (Button) findViewById(R.id.btnShowContacts);
+
+        dbHelper = new DBHandler(this);
+        db = dbHelper.getWritableDatabase();
+
+        btnMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activityMain();
+            }
+        });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Contact newContact = new Contact();
+                newContact.first = firstName.getText().toString();
+                newContact.last = lastName.getText().toString();
+                newContact.phone = phone.getText().toString();
+
+                System.out.println(newContact.first + " " + newContact.last + " " + newContact+phone);
+
+                dbHelper.addContact(db, newContact);
+            }
+        });
+
+        btnShowContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String output = "";
+                List<Contact> contactList= dbHelper.retrieveAllContacts(db);
+                for (Contact myContact : contactList){
+                    /*
+                    output += "Id: "+ myContact.get_ID() +
+                            ", Fornavn: "+myContact.getFirst() +
+                            ", Etternavn: "+ myContact.getLast() +
+                            ", Telefonnummer: "+ myContact.getPhone() + "\n";
+                    */
+                    output += myContact.get_ID() + ", "+ myContact.getFirst() + " " + myContact.getLast() + ", " + myContact.getPhone() + "\n";
+                }
+                contactOutput.setText(output);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        dbHelper.close();
+        super.onDestroy();
+    }
+
+    private void activityMain() {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+    }
+}
